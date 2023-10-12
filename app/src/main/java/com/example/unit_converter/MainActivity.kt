0a169b6d6f368b0c15cb47c04abf16b2dcfb813e
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-12 20:39:39 +0600

    Some minor changes

--- app/src/main/java/com/example/unit_converter/MainActivity.kt
+++ app/src/main/java/com/example/unit_converter/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.unit_converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var edt : TextView = findViewById(R.id.inputValue)
+        var btn : Button = findViewById(R.id.convertButton)
+        var resultText : TextView = findViewById(R.id.resultTextView)
+
+        btn.setOnClickListener(){
+            var kg = edt.text.toString().toDouble()
+
+            resultText.setText(""+convertToPounds(kg)+"\n Pounds")
+        }
+    }
+
+    fun convertToPounds(kg: Double): Double{
+         var pounds = kg * 2.20462
+
+        return pounds
     }
 }
